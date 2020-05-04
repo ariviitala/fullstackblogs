50fc7e2198d6beb1f334c5bcb555eba8134380de
Ari <amt.viitala@gmail.com>
2020-05-04 09:27:07 +0300

    Api testing exercises done

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -18,9 +18,15 @@ describe('Blog routing', () => {
     await Promise.all(promiseArray)
   })
 
-  test('Two blogs are returned', async () => {
+  test('Blogs have id-field', async () => {
+    const blogs = await api.get('/api/blogs')
+    //console.log(blogs.body)
+    blogs.body.forEach(b => expect(b.id).toBeDefined())
+    //expect(blogs[0].id).toBeDefined()
+  })
+
+  test('Enough blogs are returned', async () => {
     const response = await api.get('/api/blogs')
-    //console.log('The response' + response.contents)
     //const contents = response.body.map(r => r.content)
     expect(response.body).toHaveLength(testBlogs.length)
   })
@@ -32,12 +38,49 @@ describe('Blog routing', () => {
       .expect('Content-Type', /application\/json/)
   })
 
-  /* test('Blogs have id-field', async () => {
-    const blogs = await api.get('/api/blogs').body
+  test('Blogs can be added', async () => {
+    const newBlog = {
+      title: 'Post Test Blog',
+      author: 'Test Author',
+      url: 'Test url',
+      likes: 3
+    }
 
-    blogs.forEach(b => expect(b.id).toBeDefined())
-    //expect(blogs[0].id).toBeDefined()
-  }) */
+    await api.post('/api/blogs').send(newBlog)
+
+    const result = await api.get('/api/blogs')
+    const blogs = result.body
+
+    expect(blogs).toHaveLength(testBlogs.length + 1)
+    expect(blogs.filter(b => b.title === 'Post Test Blog')).toHaveLength(1)
+
+  })
+
+  test('Default likes zero', async () => {
+    const newBlog = {
+      title: 'Likes Test Blog',
+      author: 'Test Author',
+      url: 'Test url'
+    }
+
+    await api.post('/api/blogs').send(newBlog)
+
+    const result = await api.get('/api/blogs')
+    //console.log(result.body)
+    const blog = result.body.find(b => b.title === 'Likes Test Blog')
+    //console.log(blog)
+    expect(blog.likes).toBe(0)
+
+
+  })
+
+  test('Adding invalid blog', async () => {
+    const newBlog = {
+      author: 'Test Author'
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  })
 
 
 })
