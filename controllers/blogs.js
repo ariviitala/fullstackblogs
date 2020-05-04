f5502f41edca5dbdb7a0fd91d4f27e1d5a9f8253
Ari <amt.viitala@gmail.com>
2020-05-04 09:55:50 +0300

    Added a couple of features

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -19,7 +19,25 @@ blogsRouter.post('', async (request, response, next) => {
   } catch(error) {
     next(error)
   }
+})
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch(error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const newLikes = { likes: request.body.likes }
+    const newBlog = await Blog.findByIdAndUpdate(request.params.id, newLikes, { new: true })
+    response.json(newBlog)
+  } catch(error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
